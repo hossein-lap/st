5cb41defaab1da9825105d20f762020316d43313
hossein <hosteam01@gmail.com>
2023-08-26 02:51:39 +0330

    applied scrollback-0.8.5 patch

--- config.def.h
+++ config.def.h
@@ -206,6 +206,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ControlMask,          XK_F11,         changealpha,    {.f = -0.05} },
 	{ ControlMask,          XK_F12,         changealpha,    {.f = +0.05} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
