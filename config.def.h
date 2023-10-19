6eb0c848fb3bf4bb8ce22b1f523783a756378fda
hossein <hosteam01@gmail.com>
2023-10-19 14:35:19 +0330

    applied `scrollback-0.8.5` patch

--- config.def.h
+++ config.def.h
@@ -198,14 +198,16 @@ static Shortcut shortcuts[] = {
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
 	{ MODKEY,               XK_s,           changealpha,    {.f = -0.05} },
 	{ MODKEY,               XK_a,           changealpha,    {.f = +0.05} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
+	{ MODKEY,               XK_equal,       zoom,           {.f = +1} },
+	{ MODKEY,               XK_minus,       zoom,           {.f = -1} },
+	{ MODKEY|ControlMask,   XK_equal,       zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,              XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ TERMMOD,              XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
